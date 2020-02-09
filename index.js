eb34cc99b4b651c1777df4028b8ba1be4b2f7acc
24HourDeveloper <mikeitt009@gmail.com>
2020-02-08 19:39:47 -0500

    added express.static , added nodemon, added imgUrl, id to president object, added route for returning single president

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("assets"));
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
 const presidents = [
   {
+    id:1,
     name: "George Washington",
     height: "6' 2",
+    imgUrl:"http://localhost:5000/images/president1.jpg",
     dob: "February 22, 1732",
     died: "December 14, 1799",
     presidentNumber: 1,
@@ -13,8 +20,10 @@ const presidents = [
     impeached: false
   },
   {
+    id:2,
     name: "John Adams",
     height: "5' 7",
+    imgUrl: "http://localhost:5000/images/president2.jpg",
     dob: "October 30, 1735",
     died: "July 4, 1826",
     presidentNumber: 2,
@@ -23,8 +32,10 @@ const presidents = [
     impeached: false
   },
   {
+    id:3,
     name: "Thomas Jefferson",
     height: "6' 2",
+    imgUrl: "http://localhost:5000/images/president3.jpg",
     dob: "April 13, 1743",
     died: "July 4, 1826",
     presidentNumber: 3,
@@ -33,8 +44,10 @@ const presidents = [
     impeached: false
   },
   {
+    id:4,
     name: "James Madison Jr.",
     height: "5' 4",
+    imgUrl: "http://localhost:5000/images/president4.jpg",
     dob: "March 16, 1751",
     died: "June 28, 1836",
     presidentNumber: 4,
@@ -43,8 +56,10 @@ const presidents = [
     impeached: false
   },
   {
+    id:5,
     name: "James Monroe",
     height: "6'",
+    imgUrl: "http://localhost:5000/images/president5.jpg",
     dob: "April 28, 1758",
     died: "July 4, 1831",
     presidentNumber: 5,
@@ -58,4 +73,9 @@ app.get("/", (req, res) => {
   res.json(presidents);
 });
 
-app.listen(3000, () => console.log("server is up"));
+app.get("/president/:id", (req, res) =>{
+  const id = req.params.id;
+  res.json(presidents[id]);
+})
+
+app.listen(5000, () => console.log("server is up"));
