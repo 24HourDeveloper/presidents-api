b46dd5933b37225e9c9c47cf0c176a7deb00b84c
Michael Moore <mikeitt009@gmail.com>
2020-02-10 08:20:53 -0500

    added 20 more presidents to sql script, 20 more images, installed mysql2 package and created connection to mysql server

--- index.js
+++ index.js
@@ -1,83 +1,32 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const mysql2 = require("mysql2");
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.static("assets"));
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-const presidents = [
-  {
-    id:1,
-    name: "George Washington",
-    height: "6' 2",
-    imgUrl:"http://localhost:5000/images/president1.jpg",
-    dob: "February 22, 1732",
-    died: "December 14, 1799",
-    presidentNumber: 1,
-    term: "April 30, 1789 – March 4, 1797",
-    birthPlace: "Westmoreland County, Virginia",
-    impeached: false
-  },
-  {
-    id:2,
-    name: "John Adams",
-    height: "5' 7",
-    imgUrl: "http://localhost:5000/images/president2.jpg",
-    dob: "October 30, 1735",
-    died: "July 4, 1826",
-    presidentNumber: 2,
-    term: "March 4, 1797 – March 4, 1801",
-    birthPlace: "Braintree, Massachusetts",
-    impeached: false
-  },
-  {
-    id:3,
-    name: "Thomas Jefferson",
-    height: "6' 2",
-    imgUrl: "http://localhost:5000/images/president3.jpg",
-    dob: "April 13, 1743",
-    died: "July 4, 1826",
-    presidentNumber: 3,
-    term: "March 4, 1801 – March 4, 1809",
-    birthPlace: "Shadewell, Virginia",
-    impeached: false
-  },
-  {
-    id:4,
-    name: "James Madison Jr.",
-    height: "5' 4",
-    imgUrl: "http://localhost:5000/images/president4.jpg",
-    dob: "March 16, 1751",
-    died: "June 28, 1836",
-    presidentNumber: 4,
-    term: "March 4, 1809 – March 4, 1817",
-    birthPlace: "Port Conway, Virginia",
-    impeached: false
-  },
-  {
-    id:5,
-    name: "James Monroe",
-    height: "6'",
-    imgUrl: "http://localhost:5000/images/president5.jpg",
-    dob: "April 28, 1758",
-    died: "July 4, 1831",
-    presidentNumber: 5,
-    term: "March 4, 1817 – March 4, 1825",
-    birthPlace: "Westmoreland County, Virginia",
-    impeached: false
-  }
-];
-
-app.get("/", (req, res) => {
-  res.json(presidents);
+const connection = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Andr0id30",
+  database: "presidents"
 });
 
-app.get("/president/:id", (req, res) =>{
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM `president`", (err, results) => {
+    if (err) console.log(err);
+
+    console.log(results);
+    res.json(results);
+  });
+});
+
+app.get("/president/:id", (req, res) => {
   const id = req.params.id;
   res.json(presidents[id]);
-})
+});
 
 app.listen(5000, () => console.log("server is up"));
--- presidents.sql
+++ presidents.sql
@@ -14,48 +14,99 @@
 
 -- Dumping database structure for presidents
 DROP DATABASE IF EXISTS `presidents`;
-CREATE DATABASE IF NOT EXISTS `presidents` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE
+IF NOT EXISTS `presidents` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `presidents`;
 
 -- Dumping structure for table presidents.president
 DROP TABLE IF EXISTS `president`;
-CREATE TABLE IF NOT EXISTS `president` (
+CREATE TABLE
+IF NOT EXISTS `president`
+(
   `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `height` varchar(50) NOT NULL,
-  `img_url` varchar(50) NOT NULL,
+  `first_name` varchar
+(50) NOT NULL,
+  `last_name` varchar
+(50) NOT NULL,
+  `height` varchar
+(50) NOT NULL,
+  `img_url` varchar
+(50) NOT NULL,
   `dob` timestamp NOT NULL,
   `dod` timestamp NULL DEFAULT NULL,
   `number` int NOT NULL,
-  `birth_place` varchar(50) NOT NULL,
-  `impeached` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `name` (`first_name`,`last_name`)
+  `birth_place` varchar
+(50) NOT NULL,
+  `impeached` tinyint
+(1) DEFAULT '0',
+  PRIMARY KEY
+(`id`),
+  KEY `name`
+(`first_name`,`last_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table presidents.president: ~1 rows (approximately)
 /*!40000 ALTER TABLE `president` DISABLE KEYS */;
-INSERT INTO `president` (`id`, `first_name`, `last_name`, `height`, `img_url`, `dob`, `dod`, `number`, `birth_place`, `impeached`) VALUES
-	(1, 'George', 'Washington', '99', 'rhrdhbrhbb', '2020-02-09 11:27:12', '2020-02-09 11:27:20', 1, 'virginia', 0);
+INSERT INTO `president` (
+`id`,
+`first_name`
+, `last_name`, `height`, `img_url`, `dob`, `dod`, `number`, `birth_place`, `impeached`) VALUES
+(1, 'George', 'Washington', '74', 'http://localhost:5000/images/president1.jpg', '1732-02-22', '1799-12-14', 1, 'Westmoreland County, Virginia', 0),
+(2, 'John', 'Adams', '68', 'http://localhost:5000/images/president2.jpg', '1735-10-30', '1826-07-04', 2, 'Braintree, Massachusetts', 0),
+(3, 'Thomas', 'Jefferson', '74', 'http://localhost:5000/images/president3.jpg', '1743-04-13', '1826-07-04', 3, 'Shadewell, Virginia', 0),
+(4, 'James', 'Madison Jr', '65', 'http://localhost:5000/images/president4.jpg', '1751-03-16', '1836-06-28', 4, 'Port Conway, Virginia', 0),
+(5, 'James', 'Monroe', '72', 'http://localhost:5000/images/president5.jpg', '1758-04-28', '1831-07-04', 5, 'Westmoreland County, Virginia', 0),
+(6, 'John Quincy', 'Adams', '68', 'http://localhost:5000/images/president6.jpg', '1767-04-11', '1848-02-23', 6, 'Braintree, Massachusetts', 0),
+(7, 'Andrew','Jackson','73','http://localhost:5000/images/president7.jpg','1767-03-15','1845-06-08',7,'Waxhaws, South Carolina',0),
+(8, 'Martin Van', 'Buren', '66', 'http://localhost:5000/images/president8.jpg','1782-12-05','1862-07-24',8,'Kinderhook, New York',0),
+(9, 'William Henry','Harrison','68', 'http://localhost:5000/images/president9.jpg','1773-02-09','1841-04-04',9,'Richmond, Virginia',0),
+(10, 'John','Tyler','72','http://localhost:5000/images/president10.jpg','1790-03-29','1862-01-18',10,'Charles City, Virginia',0),
+(11, 'James Knox', 'Polk', '68','http://localhost:5000/images/president11.jpg','1795-11-02','1849-06-15',11,'Pineville, North Carolina',0),
+(12, 'Zachary','Taylor','68','http://localhost:5000/images/president12.jpg','1784-11-24','1850-07-09',12,'Barboursville, Virginia',0),
+(13,'Millard','Fillmore','69','http://localhost:5000/images/president13.jpg','1800-01-07','1874-07-09',13,'Moravia, New York',0),
+(14, 'Franklin','Pierce','70','http://localhost:5000/images/president14.jpg','1804-11-23','1869-10-08',14,'Hillsborough, New Hampshire',0),
+(15, 'James','Buchanan Jr.','72','http://localhost:5000/images/president15.jpg','1791-04-23','1868-06-01',15,'Cove Gap, Pennsylvania',0),
+(16, 'Abraham','Lincolln','76','http://localhost:5000/images/president16.jpg','1809-02-12','1865-04-15',16,'Sinking Spring Farm, Kentucky',0),
+(17, 'Andrew','Johnson','70','http://localhost:5000/images/president17.jpg','1808-12-29','1875-07-31',17,'Raleigh, North Carolina',0),
+(18, 'Ulysess S','Grant','68','http://localhost:5000/images/president18.jpg','1822-04-27','1885-07-23',18,'Point Pleasant, Ohio',0),
+(19, 'Rutherford Birchard','Hayes','69','http://localhost:5000/images/president19.jpg','1822-10-04','1893-01-17',19,'Delaware, Ohio',0),
+(20, 'James Abram','Garfield','72','http://localhost:5000/images/president20.jpg','1831-11-19','1881-11-19',20,'Moreland Hills, Ohio',0),
+(21, 'Chester Alan','Arthur','72','http://localhost:5000/images/president21.jpg','1829-10-05','1886-11-18',21,'Fairfield, Vermont',0),
+(22,  'Grover','Cleveland','71','http://localhost:5000/images/president22.jpg','1837-03-18','1908-06-24',22,'Caldwell, New Jersey',0),
+(23, 'Benjamin','Harrison','66','http://localhost:5000/images/president23.jpg','1833-08-20','1901-03-13',23,'North Bend, Ohio',0),
+(24, 'Grover','Cleveland','71','http://localhost:5000/images/president24.jpg','1837-03-18','1908-06-24',24,'Caldwell, New Jersey',0),
+(25, 'William','Mckinley','67','http://localhost:5000/images/president25.jpg','1843-01-29','1901-09-14',25,'Niles, Ohio',0);
+
 /*!40000 ALTER TABLE `president` ENABLE KEYS */;
 
 -- Dumping structure for table presidents.term
 DROP TABLE IF EXISTS `term`;
-CREATE TABLE IF NOT EXISTS `term` (
+CREATE TABLE
+IF NOT EXISTS `term`
+(
   `id` int NOT NULL AUTO_INCREMENT,
   `president_id` int NOT NULL,
   `start_date` timestamp NOT NULL,
   `end_date` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `president_id` (`president_id`),
-  KEY `start_end_date` (`start_date`,`end_date`)
+  PRIMARY KEY
+(`id`),
+  KEY `president_id`
+(`president_id`),
+  KEY `start_end_date`
+(`start_date`,`end_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Dumping data for table presidents.term: ~0 rows (approximately)
 /*!40000 ALTER TABLE `term` DISABLE KEYS */;
-INSERT INTO `term` (`id`, `president_id`, `start_date`, `end_date`) VALUES
-	(1, 1, '2020-02-09 11:29:37', '2020-02-09 11:29:38');
+INSERT INTO `term` (
+`id`,
+`president_id`
+, `start_date`, `end_date`) VALUES
+(1, 1, '1789-04-30', '1797-03-04'),
+(2, 2, '1797-03-04', '1801-03-04'),
+(3, 3, '1801-03-04', '1809-03-04'),
+(4, 4, '1809-03-04', '1817-03-04'),
+(5, 5, '1817-03-04', '1825-03-04');
 /*!40000 ALTER TABLE `term` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
